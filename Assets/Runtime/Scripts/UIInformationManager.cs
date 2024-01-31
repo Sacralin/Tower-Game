2826507057dea19aaabb9fd0b12a91a7c33b589f
Paul Mould <PM902@student.aru.ac.uk>
2024-01-31 12:09:20 +0000

    Added Gold over time

--- Assets/Runtime/Scripts/UIInformationManager.cs
+++ Assets/Runtime/Scripts/UIInformationManager.cs
@@ -8,6 +8,8 @@ public class UIInformationManager : MonoBehaviour
 {
     public TMP_Text goldText;
     public TMP_Text livesText;
+    private int goldOverTime = 1;
+    private float currentGold = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class UIInformationManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentGold += goldOverTime * Time.deltaTime;
+        goldText.text = $"Gold: {(int)currentGold}";
     }
 }
